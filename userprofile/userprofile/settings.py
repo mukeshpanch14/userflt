b0af555a59038c89462911a1e12242c367e1a875
panch mukesh <mukesh.panch14@gmail.com>
2021-01-11 23:44:35 +0530

    1

--- userprofile/userprofile/settings.py
+++ userprofile/userprofile/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&(7fv6-xjds(*(zg6aii8yp)1oi4ua@-t2rfkl*14gx%_^9d=d'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
